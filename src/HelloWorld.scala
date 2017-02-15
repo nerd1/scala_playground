a2b90217d7d84e68edb8b2d1c29687e818c8de10
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-02-15 21:35:41 +0100

    first commands

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -2,9 +2,42 @@ object HelloWorld {
   def main(args: Array[String]) {
     println("Hello, world!")
 
-    val number = 1 + 1
+    var number = 1 + 1
 
     println("number => " + number)
+
+    if (number == 2) {
+      println("number is 2")
+    } else {
+      number = number + 1
+    }
+
+
+    for (i <- 1 to 10) {
+      print(i + ", ")
+    }
+    println()
+
+    for (
+      i <- 1 to 10
+      if i % 2 == 0
+    ) {
+      print(i + ", ")
+    }
+    println()
+
+
+    def abs(x: Int) = if (x >= 0) x else -x
+
+    println("abs:"+abs(-5))
+
+
+    val fruits = Array("Apple", "Banana", "Orange")
+
+    for (fruit <- fruits) {
+      print(fruit + ", ")
+    }
+    println()
 
   }
 }
